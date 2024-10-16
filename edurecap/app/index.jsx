a15c963a0f66613eef0ea67f8d2fd5ca91103710
LAKSHMI GOPINATH <lakshmigopinath21@gmail.com>
2024-10-16 12:10:51 +0530

    Add files via upload

---
+++ edurecap/app/index.jsx
@@ -0,0 +1,68 @@
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import { CustomButton, Loader } from "../components";
+import { useGlobalContext } from "../context/GlobalProvider";
+
+const Welcome = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/home" />;
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <Loader isLoading={loading} />
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[150px] h-[104px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[400px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Edurecap</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-0"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Record. Recap. Master.
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
---
+++ edurecap/lib/appwrite.js
@@ -0,0 +1,269 @@
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Query,
+  Storage,
+} from "react-native-appwrite";
+
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.json.edurecap",
+  projectId: "66b26ead0019b0e69387",
+  storageId: "66b271d4001bcfb6fefd",
+  databaseId: "66b27030003042c0e345",
+  userCollectionId: "66b27054003c2b13eb86",
+  videoCollectionId: "66b27093002a938136e9",
+};
+
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+const account = new Account(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+// Register user
+export async function createUser(email, password, username) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw new Error("Failed to create account");
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email: email,
+        username: username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Sign In
+export async function signIn(email, password) {
+  try {
+    const session = await account.createEmailSession(email, password);
+    return session;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Check if user is authenticated
+export async function isAuthenticated() {
+  try {
+    const session = await account.get();
+    return !!session;
+  } catch (error) {
+    return false;
+  }
+}
+
+// Get Account
+export async function getAccount() {
+  try {
+    if (!(await isAuthenticated())) {
+      throw new Error("User is not authenticated");
+    }
+    const currentAccount = await account.get();
+    return currentAccount;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Get Current User
+export async function getCurrentUser() {
+  try {
+    if (!(await isAuthenticated())) {
+      throw new Error("User is not authenticated");
+    }
+    const currentAccount = await getAccount();
+    if (!currentAccount) throw new Error("Account not found");
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw new Error("User not found");
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Sign Out
+export async function signOut() {
+  try {
+    const session = await account.deleteSession("current");
+    return session;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Upload File
+export async function uploadFile(file, type) {
+  if (!file) return;
+
+  const { mimeType, ...rest } = file;
+  const asset = { type: mimeType, ...rest };
+
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      asset
+    );
+
+    const fileUrl = await getFilePreview(uploadedFile.$id, type);
+    return fileUrl;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Get File Preview
+export async function getFilePreview(fileId, type) {
+  let fileUrl;
+
+  try {
+    if (type === "video") {
+      fileUrl = storage.getFileView(appwriteConfig.storageId, fileId);
+    } else if (type === "image") {
+      fileUrl = storage.getFilePreview(
+        appwriteConfig.storageId,
+        fileId,
+        2000,
+        2000,
+        "top",
+        100
+      );
+    } else {
+      throw new Error("Invalid file type");
+    }
+
+    if (!fileUrl) throw new Error("File URL not found");
+
+    return fileUrl;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Create Video Post
+export async function createVideoPost(form) {
+  try {
+    const [thumbnailUrl, videoUrl] = await Promise.all([
+      uploadFile(form.thumbnail, "image"),
+      uploadFile(form.video, "video"),
+    ]);
+
+    const newPost = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.videoCollectionId,
+      ID.unique(),
+      {
+        title: form.title,
+        thumbnail: thumbnailUrl,
+        video: videoUrl,
+        prompt: form.prompt,
+        creator: form.userId,
+      }
+    );
+
+    return newPost;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Get all video Posts
+export async function getAllPosts() {
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.videoCollectionId
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Get video posts created by user
+export async function getUserPosts(userId) {
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.videoCollectionId,
+      [Query.equal("creator", userId)]
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Get video posts that matches search query
+export async function searchPosts(query) {
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.videoCollectionId,
+      [Query.search("title", query)]
+    );
+
+    if (!posts) throw new Error("Something went wrong");
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+// Get latest created video posts
+export async function getLatestPosts() {
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.videoCollectionId,
+      [Query.orderDesc("$createdAt"), Query.limit(7)]
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
